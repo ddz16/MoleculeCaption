c04818dd673e1aa522c339b8a10d7f7f4913628c
ddz16 <1170392166@qq.com>
2023-02-24 23:50:39 +0800

    update

--- model/GinT5.py
+++ model/GinT5.py
@@ -6,14 +6,18 @@ from model.gin_model import GNN
 
 
 class GinDecoder(nn.Module):
-    def __init__(self, has_graph=False, MoMuK=True, model_size='base'):
+    def __init__(self, has_graph=True, MoMuK=True, model_size='base'):
         super(GinDecoder, self).__init__()
         self.has_graph = has_graph
-        self.main_model = T5ForConditionalGeneration.from_pretrained("molt5-"+model_size+"/")
-        print(self.main_model.config.hidden_size)
+        self.main_model = T5ForConditionalGeneration.from_pretrained("molt5-"+model_size+"-smiles2caption/")
+        print(f'hidden_size: {self.main_model.config.hidden_size},\
+                d_model: {self.main_model.config.d_model},\
+                num_decoder_layers: {self.main_model.config.num_decoder_layers},\
+                num_heads: {self.main_model.config.num_heads},\
+                d_kv: {self.main_model.config.d_kv}')
 
-        # for p in self.main_model.named_parameters():
-        #     p[1].requires_grad = False
+        for p in self.main_model.named_parameters():
+            p[1].requires_grad = False
 
         if has_graph:
             self.graph_encoder = GNN(
@@ -25,110 +29,87 @@ class GinDecoder(nn.Module):
             )
             
             if MoMuK:
-                ckpt = torch.load("./checkpoints/littlegin=graphclinit_bert=kvplm_epoch=299-step=18300.ckpt")
+                ckpt = torch.load("./MoMu_checkpoints/littlegin=graphclinit_bert=kvplm_epoch=299-step=18300.ckpt")
             else:
-                ckpt = torch.load("./checkpoints/littlegin=graphclinit_bert=scibert_epoch=299-step=18300.ckpt")
+                ckpt = torch.load("./MoMu_checkpoints/littlegin=graphclinit_bert=scibert_epoch=299-step=18300.ckpt")
             ckpt = ckpt['state_dict']
             pretrained_dict = {k[14:]: v for k, v in ckpt.items()}
             missing_keys, unexpected_keys = self.graph_encoder.load_state_dict(pretrained_dict, strict=False)
 
             for p in self.graph_encoder.named_parameters():
                 p[1].requires_grad = False
-
+            
             self.graph_projector = nn.Sequential(
                 nn.Linear(300, self.main_model.config.hidden_size),
                 nn.ReLU(inplace=True),
                 nn.Linear(self.main_model.config.hidden_size, self.main_model.config.hidden_size)
             )
+            # self.graph_projector_dropout = torch.nn.Dropout(0.2)
             # self.graph_projector = nn.Linear(300, self.main_model.config.hidden_size)
 
 
-
     def forward(self, batch, input_ids, encoder_attention_mask, decoder_attention_mask, label):
-        # typ = torch.zeros(input_ids.shape).long().to(device)
-
-
-        # print("GIN output shape: ", node_reps.shape)
-        # print("T5 decoder input token shape: ", input_ids.shape)  # B,L
-        # print("T5 decoder input mask shape: ", tgt_mask.shape)  # B,L
-        # print("GIN node mask: ", encoder_attention_mask[0])
-
-        input_embeds = self.main_model.shared(input_ids)
         device = encoder_attention_mask.device
         B, _ = encoder_attention_mask.shape
 
+        smiles_embeds = self.main_model.encoder(input_ids=input_ids, attention_mask=encoder_attention_mask).last_hidden_state
+        
         if self.has_graph:
             graph_rep = self.graph_encoder(batch)
             graph_rep = self.graph_projector(graph_rep)
-            # graph_rep = torch.nn.functional.normalize(graph_rep, dim=1)
-            input_embeds = torch.cat([graph_rep.unsqueeze(1), input_embeds[:, :-1, :]], dim=1)
-            encoder_attention_mask = torch.cat([torch.ones(B, 1).to(device), encoder_attention_mask[:, :-1]], dim=1)
+            smiles_embeds = torch.cat([graph_rep.unsqueeze(1), smiles_embeds], dim=1)
+            encoder_attention_mask = torch.cat([torch.ones(B, 1).to(device), encoder_attention_mask], dim=1)
         
+        encoder_outputs = BaseModelOutput(
+                last_hidden_state=smiles_embeds,
+                hidden_states=None,
+                attentions=None,
+            )
         loss = self.main_model(
-            inputs_embeds = input_embeds,
-            attention_mask=encoder_attention_mask,
-            decoder_attention_mask=decoder_attention_mask,
-            labels=label # 有了labels就不用decoder_input_ids了，因为会自动将labels左移生成decoder_input_ids
+            encoder_outputs = encoder_outputs,
+            attention_mask = encoder_attention_mask,
+            decoder_attention_mask = decoder_attention_mask,
+            labels=label
             ).loss
 
-        # print("loss: ", loss.item())
-
         return loss
-    
+
 
     def translate(self, batch, input_ids, encoder_attention_mask, tokenizer):
-
-        input_embeds = self.main_model.shared(input_ids)
         device = encoder_attention_mask.device
         B, _ = encoder_attention_mask.shape
+    
+        smiles_embeds = self.main_model.encoder(input_ids=input_ids, attention_mask=encoder_attention_mask).last_hidden_state
 
         if self.has_graph:
             graph_rep = self.graph_encoder(batch)
             graph_rep = self.graph_projector(graph_rep)
-            input_embeds = torch.cat([graph_rep.unsqueeze(1), input_embeds[:, :-1, :]], dim=1)
-            encoder_attention_mask = torch.cat([torch.ones(B, 1).to(device), encoder_attention_mask[:, :-1]], dim=1)
+            smiles_embeds = torch.cat([graph_rep.unsqueeze(1), smiles_embeds], dim=1)
+            encoder_attention_mask = torch.cat([torch.ones(B, 1).to(device), encoder_attention_mask], dim=1)
+           
+        # input_prompt = ["The molecule is"] * B
+        # decoder_input_ids = tokenizer(input_prompt, return_tensors="pt").input_ids
+        # decoder_input_ids = decoder_input_ids.to(device)
+        num_beams = 5
 
-        # input_prompt = "The molecule is"
-        # input_ids = tokenizer(input_prompt, return_tensors="pt").input_ids
-        # input_ids = input_ids.to(device)
-
+        encoder_outputs = BaseModelOutput(
+                last_hidden_state=smiles_embeds,
+                hidden_states=None,
+                attentions=None,
+            )
         outputs = self.main_model.generate(
-            inputs_embeds=input_embeds,
-            attention_mask=encoder_attention_mask,
-            num_beams=5,
-            # bos_token_id=102,
+            encoder_outputs = encoder_outputs,  
+            attention_mask = encoder_attention_mask,  # important
+            num_beams=num_beams,
             max_length=512,
+            # eos_token_id=self.main_model.config.eos_token_id,
+            # decoder_start_token_id=self.main_model.config.decoder_start_token_id,
+            # decoder_input_ids = decoder_input_ids,
         )
 
         res = tokenizer.batch_decode(outputs, skip_special_tokens=True)
         return res
 
-
-
-    # def forward(self, batch, input_ids, tgt_mask):
-    #     device = input_ids.device
-    #     typ = torch.zeros(input_ids.shape).long().to(device)
-
-    #     node_reps, encoder_attention_mask = self.graph_encoder(batch)
-    #     encoder_attention_mask = encoder_attention_mask.to(device)
-    #     print("GIN output shape: ", node_reps.shape)
-    #     print("T5 decoder input token shape: ", input_ids.shape)  # B,L
-    #     print("T5 decoder input mask shape: ", tgt_mask.shape)  # B,L
-    #     # print("GIN node mask: ", encoder_attention_mask[0])
-
-    #     node_reps = self.graph_projector(node_reps)
-
-    #     output = self.main_model(
-    #         encoder_outputs=node_reps.unsqueeze(0), 
-    #         decoder_input_ids=input_ids, 
-    #         attention_mask=encoder_attention_mask,
-    #         decoder_attention_mask=tgt_mask,
-    #         )["last_hidden_state"]
-
-    #     print("T5 decoder output shape: ", output.shape)
-
-    #     return output
-    
     def gin_encode(self, batch):
         node_reps = self.graph_encoder(batch)
         return node_reps
